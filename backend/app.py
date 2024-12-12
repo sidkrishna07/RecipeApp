d982329a570d2d83225997ff4923cb2ddffadf40
Aravind763 <aravind.bala36@gmail.com>
2024-12-11 18:40:32 -0800

    push new

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_cors import CORS
 from datetime import timedelta
-#---testt
+#
 # Initialize the Flask app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
